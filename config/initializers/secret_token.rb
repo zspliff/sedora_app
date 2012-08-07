c8ec1544a854678a9556e306555533e958b3e5e6
zspliff <zspliff@gmail.com>
2012-08-07 02:36:09 -0400

    Start > again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sedora::Application.config.secret_token = 'c212a2c8ce1fb90e029a70e70742c4ce7d5fc03c03f9017600aeca66d2f1b43714e74038ef3c2ecbc2ec2c1c3e48b3caccae1888576a22e96fc725a52cb4fba0'
